cbd4d5d90a1023d714211c6528c60d44989a910b
Luaxwing <nick890111@gmail.com>
2024-02-06 15:58:12 +0800

    20240206

---
+++ o20240206/myAPI/weatherJS/js.js
@@ -0,0 +1,3 @@
+let weatherUrl = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-EB2FA7D0-3AF6-41AD-8C52-5B32DE081F8D`;
+
+let rainUrl=`https://opendata.cwa.gov.tw/api/v1/rest/datastore/C-B0025-001?Authorization=CWA-EB2FA7D0-3AF6-41AD-8C52-5B32DE081F8D`;
